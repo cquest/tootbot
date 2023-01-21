7d03160f43b56cba2c79292f6144d0e0fbfa7285
Raphael Fetzer <58503+pheraph@users.noreply.github.com>
2023-01-21 13:48:38 +0100

    Customizable visibility of toots (public/unlisted)

--- cron-sample.sh
+++ cron-sample.sh
@@ -8,8 +8,10 @@
 # 4- instance domain (https:// is automatically added)
 # 5- max age (in days)
 # 6- footer tags to add (optional)
+# 7- delay (optional)
+# 8- visibility unlisted (if 0 or unset visibility is public, if 1 visibility is unlisted)
 
 python3 tootbot.py geonym_fr geonym@amicale.net **password** test.amicale.net
 python3 tootbot.py cq94 cquest@amicale.net **password** test.amicale.net
 
-python3 tootbot.py https://www.data.gouv.fr/fr/datasets/recent.atom cquest+opendata@amicale.net **password** amicale.net 2 "#dataset #opendata #datagouvfr"
+python3 tootbot.py https://www.data.gouv.fr/fr/datasets/recent.atom cquest+opendata@amicale.net **password** amicale.net 2 "#dataset #opendata #datagouvfr" 0 1
